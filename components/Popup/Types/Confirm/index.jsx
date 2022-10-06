107604e718b51bcde23b014a0a643f6e20853c21
Anna Lozovaya <anyalozovaya2014@gmail.com>
2022-10-06 21:08:38 +0300

    refactor: components Popup and types of Popup

--- components/Popup/Types/Confirm/index.jsx
+++ components/Popup/Types/Confirm/index.jsx
@@ -1,39 +1,24 @@
-import Modal from 'react-modal';
-
 import { Confirm } from './Confirm';
 import { Button } from '@components';
 
 import styles from './style.module.scss';
 
-const ConfirmPopup = ({ children, isOpen, toggle, buttonPopupConfirm, buttonPopupDelete, ...props }) => {
+const ConfirmPopup = ({ children, toggle, buttonPopupConfirm, buttonPopupDelete,}) => {
 	return (
-		<Modal
-			isOpen={isOpen}
-			onRequestClose={toggle}
-			className={styles.popup}
-			overlayClassName={ styles.popup_overlay }
-			closeTimeoutMS={0}
-        	ariaHideApp={false}
-            { ...props }>
-            
-            <div className={ styles['popup-confirm'] }>
-                <div className={ styles['popup-confirm_circle'] }>
-                    <Confirm className={styles['popup-confirm_circle__icon'] }/> 
-                </div>
-				<h2 className={ styles['popup-confirm_description'] }>{ children }</h2>
-				<div className={ styles['popup-confirm_btns'] }>
-					<Button type='default' onClick={toggle}>
-						{buttonPopupConfirm}
-					</Button>
-					<Button type='border' onClick={toggle}>
-						{buttonPopupDelete}
-					</Button>
-				</div>
+		<div className={ styles['popup-confirm'] }>
+            <div className={ styles['popup-confirm_circle'] }>
+                <Confirm className={styles['popup-confirm_circle__icon'] }/> 
             </div>
-            
-            
-			
-		</Modal>
+			<h2 className={ styles['popup-confirm_description'] }>{ children }</h2>
+			<div className={ styles['popup-confirm_btns'] }>
+				<Button type='default' onClick={toggle}>
+					{buttonPopupConfirm}
+				</Button>
+				<Button type='border' onClick={toggle}>
+					{buttonPopupDelete}
+				</Button>
+			</div>
+        </div>
 	);
 };
 
--- components/Popup/Types/Success/index.jsx
+++ components/Popup/Types/Success/index.jsx
@@ -1,30 +1,15 @@
-import Modal from 'react-modal';
-
 import {Check} from './Check';
 
 import styles from './style.module.scss';
 
-const SuccessPopup = ({ children, isOpen, toggle, ...props }) => {
+const SuccessPopup = ({ children }) => {
 	return (
-		<Modal
-			isOpen={isOpen}
-			onRequestClose={toggle}
-			className={styles.popup}
-			overlayClassName={ styles.popup_overlay }
-			closeTimeoutMS={0}
-        	ariaHideApp={false}
-            { ...props }>
-            
-            <div className={ styles['popup-success'] }>
-                <div className={ styles['popup-success_circle'] }>
-                    <Check className={styles['popup-success_circle__icon'] }/> 
-                </div>
-                <h2 className={ styles['popup-success_description'] }>{ children }</h2>
+        <div className={ styles['popup-success'] }>
+            <div className={ styles['popup-success_circle'] }>
+	            <Check className={styles['popup-success_circle__icon'] }/> 
             </div>
-            
-            
-			
-		</Modal>
+            <h2 className={ styles['popup-success_description'] }>{ children }</h2>
+        </div>
 	);
 };
 
